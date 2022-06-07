c6d01661645637c19365ab80f5dfe6f4be5dff08
maher <gomemahero.com>
2022-06-07 12:05:53 +0300

    drop-down 2  with images condition is workk

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,166 +1,163 @@
-import React from 'react';
-import { categories } from '../redux/data';
+import React from "react";
+import { categories, categories2 } from "../redux/data";
 const Navbar = () => {
-    return (
-        <div>
-            
-<div className=' h-[80px]    shadow-2xl'>
+  return (
+    <div>
+      <div className=" h-[80px]    shadow-2xl">
+        <div className=" container h-full  ml-6  pb-4  content-end mb-[-6px] grid grid-cols-12">
+          {/* -------logo left----- */}
 
-<div className=' container h-full  ml-6  pb-4  content-end mb-[-6px] grid grid-cols-12'>
+          <div className=" col-span-2">
+            <div className="  w-[80px] h-[64px]">
+              <img
+                src="https://cdn0.iconfinder.com/data/icons/logos-21/40/Adidas-256.png"
+                alt=""
+              />
+            </div>
+          </div>
 
-{/* -------logo left----- */}
+          {/* middle- */}
 
-<div className=' col-span-3'>
-    
-<div
-className='  w-[80px] h-[64px]'
->
-    <img src="https://cdn0.iconfinder.com/data/icons/logos-21/40/Adidas-256.png" alt="" />
+          <div className="  col-span-6">
+            <div className="flex sm:gap-2  lg:gap-8 ">
+              {/* -----1---- */}
+              <div className="w-1/2   transform  translate-y-[30px]">
+                <ul className=" flex  gap-8">
+                  {categories.map((cat) => (
+                    <li className="    group  font-semibold text-[17px]">
+                      <p className=" hover:border-b-4 hover:border-indigo-500  duration-200  transition-all ">
+                        {cat.name}
+                      </p>
+
+                      {/* -submenu- */}
+
+                      <div className="absolute w-[77vw] left-[-213px]  min-h-[300px]   shadow-2xl  bg-white    z-30 hidden  group-hover:block  top-10 transition-all   duration-300">
+                        {/* ----content-start---- */}
+
+                        <div className=" container">
+                          <div>
+                            <ul className=" mt-12 flex justify-between w-[85%] mx-auto">
+                              {cat.submenu.map((sub) => (
+                                <li>
+                                  <p>{sub.name}</p>
+
+                                  <div>
+                                    <ul>
+                                      {sub.list.map((item) => (
+                                        <div>
+                                          <li className=" text-sm">
+                                            <p>{item}</p>
+                                          </li>
+                                        </div>
+                                      ))}
+                                    </ul>
+                                  </div>
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+
+              {/* -----2---- */}
+              <div>
+                <div className="  translate-y-[30px]  ">
+                  <div>
+                    <ul className="  flex gap-6 ">
+                      {categories2.map((cat2) => (
+                        <li className=" text-[17px]  group font-semibold">
+                          <p className="  group">{cat2.name}</p>
+
+                          {/* -submenu- */}
+
+                          <div className="absolute w-[77vw] left-[-545px]  min-h-[300px]   shadow-2xl  bg-white    z-30 hidden  group-hover:block  top-10 transition-all   duration-300">
+                            {/* ----dontent-start---- */}
+
+                            <div className=" container  mt-12">
+                              <div>
+                                <ul className=" flex justify-between   w-[80%]  mx-auto  ml-[50px]">
+                                  {cat2.submenu.map((sub) => (
+                                    <div>
+                                      <li>
+                                        <p>
+                                          {!sub.name == "" ? (
+                                            <div>
+                                              <div>
+                                                <p>{sub.name}</p>
+                                              </div>
+
+                                              <div>
+
+<div  className="w-[170px]   h-[82px]">
+    <img  className=" w-full  h-full" src={sub.image1} alt="" />
 </div>
 
+
+
 </div>
+                                            </div>
+                                          ) : (
+                                            <div>
+                                              <div>
+                                                <img src={sub.image1} alt="" />
+                                              </div>
+
+                                              {/* -img2-- */}
+
+                                              <div className=" w-[170px]   h-[82px]">
+                                                <img
+                                                  className=" w-full h-full object-cover"
+                                                  src={sub.image2}
+                                                  alt=""
+                                                />
+                                              </div>
+                                            </div>
+                                          )}
+
+<ul className=" text-center">
+
+{sub.list.map((item) => (<li>
 
 
+<p>
+{item}
+</p>
 
 
-
-{/* middle- */}
-
-<div className='  col-span-6'>
-    
-<div
-
-className='flex  gap-12 '
->
-
-{/* -----1---- */}
-<div className='w-1/2   transform  translate-y-[30px]'>
-
-
-<ul className=' flex  gap-8'>
-
-{categories.map(cat => (
-
-
-<li className='    group  font-semibold text-[17px]'>
-
-<p
-
-className=' hover:border-b-4 hover:border-indigo-500  duration-200  transition-all '
->{cat.name}</p>
-
-
-{/* -submenu- */}
-
-<div className='absolute w-[77vw] left-[-334px]  min-h-[300px]   shadow-2xl  bg-white    z-30 hidden  group-hover:block  top-10 transition-all   duration-300'>
-
-
-{/* ----content-start---- */}
-
-
-<div className=' container'>
-
-
-<div>
-
-<ul className=' mt-12 flex justify-between w-[85%] mx-auto'>
-
-
-{cat.submenu.map(sub => (
-
-<li>
-    <p>
-    {sub.name}
-    </p>
-
-
-<div>
-
-<ul>
-
-{sub.list.map(item => (<div>
-
-<li className=' text-sm'>
-    <p>{item}</p>
-</li>
-
-
-
-</div>))}
-
+</li>))}
 
 </ul>
 
 
 
-</div>
+                                        </p>
+                                      </li>
+                                    </div>
+                                  ))}
+                                </ul>
+                              </div>
+                            </div>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
 
+          {/* ---right---- */}
 
-
-
-</li>
-))}
-
-</ul>
-
-
-</div>
-
-
-
-</div>
-
-
-
-</div>
-
-
-
-</li>) )}
-
-
-
-</ul>
-
-
-
-
-</div>
-
-{/* -----2---- */}
-<div>
-    2
-</div>
-
-
-
-
-</div>
-
-
-
-</div>
-
-
-
-
-{/* ---right---- */}
-
-<div className='  col-span-3'>
-    3
-</div>
-
-
-</div>
-
-
-
-
-</div>
-
-
+          <div className="  col-span-4">3</div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
--- src/redux/data.js
+++ src/redux/data.js
@@ -1,196 +1,327 @@
 export const categories = [
-
-{
+  {
     id: 1,
-    name:'erkek'    ,
-    submenu:[
+    name: "erkek",
+    submenu: [
+      // ----1---
 
-// ----1---
+      {
+        id: 1,
+        name: "ayakkabı",
+        list: [
+          "yeni urunler",
+          "kosu",
+          "Training",
+          " Outdoor",
+          "sport inspired",
+          "Tenis",
+          "Terlik",
+        ],
+      },
 
-{
+      //----2-------
+      {
+        id: 2,
+        name: "Giyim",
+        list: [
+          ,
+          "tshir",
+          "form",
+          "kapsonlu",
+          "esofman alt",
+          "tayit",
+          "mont",
+          "short",
+          "mayo",
+        ],
+      },
 
+      //---3----
+
+      {
+        id: 3,
+        name: "accessories",
+        list: [
+          "maskeler",
+          "cantalar",
+          "sirt cantalar",
+          "antrenman cantalar",
+          "corap",
+        ],
+      },
+      {
+        id: 4,
+        name: "spor category",
+        list: [, "futbol", "basketball", "tennis", "outdoor", "yuzme", "kosu"],
+      },
+    ],
+  },
+
+  //---------------------------
+  {
     id: 1,
-    name:'ayakkabı',
-    list:[
-        "yeni urunler",  "kosu","Training"," Outdoor","sport inspired","Tenis","Terlik",
+    name: "Cocuk",
 
-    ]
-}
+    submenu: [
+      // ----1---
 
+      {
+        id: 1,
+        name: "ayakkabı",
+        list: [
+          "yeni urunler",
+          "kosu",
+          "Training",
+          " Outdoor",
+          "sport inspired",
+          "Tenis",
+          "Terlik",
+        ],
+      },
 
+      //----2-------
+      {
+        id: 2,
+        name: "Giyim",
+        list: [
+          ,
+          "tshir",
+          "form",
+          "kapsonlu",
+          "esofman alt",
+          "tayit",
+          "mont",
+          "short",
+          "mayo",
+        ],
+      },
 
-//----2-------
-,
-{
+      //---3----
 
-id:2,
-name:'Giyim',
-list:[
-    ,"tshir","form","kapsonlu","esofman alt","tayit","mont","short","mayo"
+      {
+        id: 3,
+        name: "accessories",
+        list: [
+          "maskeler",
+          "cantalar",
+          "sirt cantalar",
+          "antrenman cantalar",
+          "corap",
+        ],
+      },
+      {
+        id: 4,
+        name: "spor category",
+        list: [, "futbol", "basketball", "tennis", "outdoor", "yuzme", "kosu"],
+      },
+    ],
+  },
 
-]
-
-},
-
-//---3----
-
-{
-
-id:3,
-name:'accessories',
-list:[
-    "maskeler","cantalar","sirt cantalar","antrenman cantalar","corap"
-]
-
-},
-{
-
-
-    id:4,
-    name:'spor category',
-    list:[
-        ,"futbol","basketball","tennis","outdoor","yuzme","kosu",
-    ]
-}
-
-
-
-    ]
-
-
-
-
-},
-
-//---------------------------
-{
+  {
     id: 1,
-    name:'Cocuk'    ,
-    
-    submenu:[
+    name: "Kadın",
+    submenu: [
+      // ----1---
 
-        // ----1---
-        
-        {
-        
-            id: 1,
-            name:'ayakkabı',
-            list:[
-                "yeni urunler",  "kosu","Training"," Outdoor","sport inspired","Tenis","Terlik",
-        
-            ]
-        }
-        
-        
-        
-        //----2-------
-        ,
-        {
-        
-        id:2,
-        name:'Giyim',
-        list:[
-            ,"tshir","form","kapsonlu","esofman alt","tayit","mont","short","mayo"
-        
-        ]
-        
-        },
-        
-        //---3----
-        
-        {
-        
-        id:3,
-        name:'accessories',
-        list:[
-            "maskeler","cantalar","sirt cantalar","antrenman cantalar","corap"
-        ]
-        
-        },
-        {
-        
-        
-            id:4,
-            name:'spor category',
-            list:[
-                ,"futbol","basketball","tennis","outdoor","yuzme","kosu",
-            ]
-        }
-        
-        
-        
-            ]
+      {
+        id: 1,
+        name: "ayakkabı",
+        list: [
+          "yeni urunler",
+          "kosu",
+          "Training",
+          " Outdoor",
+          "sport inspired",
+          "Tenis",
+          "Terlik",
+        ],
+      },
 
+      //----2-------
+      {
+        id: 2,
+        name: "Giyim",
+        list: [
+          ,
+          "tshir",
+          "form",
+          "kapsonlu",
+          "esofman alt",
+          "tayit",
+          "mont",
+          "short",
+          "mayo",
+        ],
+      },
 
-},
+      //---3----
 
-{
+      {
+        id: 3,
+        name: "accessories",
+        list: [
+          "maskeler",
+          "cantalar",
+          "sirt cantalar",
+          "antrenman cantalar",
+          "corap",
+        ],
+      },
+      {
+        id: 4,
+        name: "spor category",
+        list: [, "futbol", "basketball", "tennis", "outdoor", "yuzme", "kosu"],
+      },
+    ],
+  },
+];
+
+// ---cat2---
+
+export const categories2 = [
+  {
     id: 1,
-    name:'Kadın'    ,
-    submenu:[
+    name: "Sporlar",
 
-        // ----1---
-        
-        {
-        
-            id: 1,
-            name:'ayakkabı',
-            list:[
-                "yeni urunler",  "kosu","Training"," Outdoor","sport inspired","Tenis","Terlik",
-        
-            ]
-        }
-        
-        
-        
-        //----2-------
-        ,
-        {
-        
-        id:2,
-        name:'Giyim',
-        list:[
-            ,"tshir","form","kapsonlu","esofman alt","tayit","mont","short","mayo"
-        
-        ]
-        
-        },
-        
-        //---3----
-        
-        {
-        
-        id:3,
-        name:'accessories',
-        list:[
-            "maskeler","cantalar","sirt cantalar","antrenman cantalar","corap"
-        ]
-        
-        },
-        {
-        
-        
-            id:4,
-            name:'spor category',
-            list:[
-                ,"futbol","basketball","tennis","outdoor","yuzme","kosu",
-            ]
-        }
-        
-        
-        
-            ]
-    
+    submenu: [
+      {
+        id: 1,
+        name: "futbol",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/football-asset-jan2022_tcm201-838384.jpeg",
+        image2: "",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa", "corap"],
+      },
+      // ---2-
 
+      {
+        id: 2,
+        name: "kosu",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/sports-running-new-ui_tcm201-789214.jpeg",
+        image2: "",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa", "corap"],
+      },
 
+      //   --3-----
 
-},
+      {
+        id: 3,
+        name: "Basketbal",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/basketball-nav-image-harden_tcm201-798830.jpg",
+        image2: "",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa", "corap"],
+      },
 
+      //   -4-
 
+      {
+        id: 4,
+        name: "Tennis",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/training-image_tcm201-789216.jpg",
+        image2: "",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa"],
+      },
+    ],
+  },
 
+  //   ----2-----
+  {
+    id: 2,
+    name: "Markaler",
 
+    submenu: [
+      {
+        id: 1,
+        name: "",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/Originals_Brand_Nav_Title_tcm201-669567.png",
+        image2:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/originals_native_display_2D_tcm201-833306.jpg",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa"],
+      },
+      {
+        id: 2,
+        name: "",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/ca-sportswear-header-AUG2021-image_tcm201-876333.png",
+        image2:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/Brand-Terrex-FiveTen_tcm201-833304.jpeg",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa"],
+      },
 
+      {
+        id: 3,
+        name: "",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/SM_tcm201-669660.png",
+        image2:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/adidas_by_stella_mccartney_tcm201-833303.jpeg",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa"],
+      },
 
+      {
+        id: 4,
+        name: "",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/y3_tcm201-669655.png",
+        image2:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/new-Y-3_resize_tcm201-833336.jpg",
+        list: ["ayakkabi", "aksesuar", "gyim", "predator", "jopa"],
+      },
+    ],
+  },
 
+  //   -4-----
 
-]
+  {
+    id: 3,
+    name: "Koleksiyonlar",
+
+    submenu: [
+      {
+        id: 1,
+        name: "One cikanlar",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/whats-new-nav-d-small_tcm201-888217.png",
+        image2: "",
+        list: ["nothing is impossible", "run for the ocans", "add club"],
+      },
+
+      {
+        id: 2,
+        name: "collaboration",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/collabaration_tcm201-888218.png",
+        image2: "",
+        list: [
+          "amrk davies",
+          "aksesuar",
+          "human made",
+          "pharriel",
+          " kolosser",
+        ],
+      },
+
+      {
+        id: 3,
+        name: "sporlar",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/sports_tcm201-888219.png",
+        image2: "",
+        list: ["predator", "adezor", "gyim", "4k"],
+      },
+
+      {
+        id: 4,
+        name: "One cikanlar",
+        image1:
+          "https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/trTR/Images/originals_tcm201-888220.png",
+        image2: "",
+        list: ["supreslar", "stani smith", "gyim", "forum", "jopa"],
+      },
+    ],
+  },
+];
